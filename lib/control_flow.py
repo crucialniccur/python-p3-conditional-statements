4f3ea9db118338d57a2e6ef84d428a3b92604225
Mati Joseph <crucialniccur@gmail.com>
2025-05-06 12:38:33 +0300

    define fizzbuzz test

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -22,8 +22,14 @@ def hows_the_weather(temperature):
 
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if (num % 3 == 0) and (num % 5 == 0):
+        return "FizzBuzz"
+    elif num % 5 == 0:
+        return "Buzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    else:
+        return num
 
 
 def calculator(operation, num1, num2):
