6403a87c391fb88d87fb37dbbf93d7a31bb54c8c
Mati Joseph <crucialniccur@gmail.com>
2025-05-06 12:19:02 +0300

    define the admin_login function

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    if (username == 'admin' or username == 'ADMIN') and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+
+
+print(admin_login("admin", "12345"))
+
 
 def hows_the_weather(temperature):
     # your code here
     pass
 
+
 def fizzbuzz(num):
     # your code here
     pass
+
 
 def calculator(operation, num1, num2):
     # your code here
